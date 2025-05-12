126a0d33201300a777825b356c426061011fca8a
Мологин Фёдор <proterrarea@gmail.com>
2025-05-12 17:41:14 +0300

    Fix random logic images

--- Cogs/Basic/Rand/random_text.py
+++ Cogs/Basic/Rand/random_text.py
@@ -19,13 +19,13 @@ bad_descriptions = [
 ]
 
 good_images = [
-    'https://cdn.discordapp.com/attachments/1266472308235571261/1309271448094773299/'
-    'a922aa26e575735d59359ce3d3ab6cf9.png?ex=6740f98e&is=673fa80e&hm=73d524bae14997a'
-    'c28a0f5a2a673e5cce3ad7765a0365743043c20b9446914fc&',
-]
-
-bad_images = [
     'https://cdn.discordapp.com/attachments/1101524758824230935/1371488190891950120/'
     'image.png?ex=68235169&is=6821ffe9&hm=0e21de457bf4c13466a6cf4129843ba8023ab2313f'
     'da5fe585e8a178cbca1ac1&'
+]
+
+bad_images = [
+    'https://cdn.discordapp.com/attachments/1266472308235571261/1309271448094773299/'
+    'a922aa26e575735d59359ce3d3ab6cf9.png?ex=6740f98e&is=673fa80e&hm=73d524bae14997a'
+    'c28a0f5a2a673e5cce3ad7765a0365743043c20b9446914fc&',
 ]
