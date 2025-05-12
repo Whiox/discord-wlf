195a3ead5b2d34da0852a805d982d31c34c72c76
Мологин Фёдор <proterrarea@gmail.com>
2025-05-12 17:43:19 +0300

    Fix all random logic names

--- Cogs/Basic/Rand/random_text.py
+++ Cogs/Basic/Rand/random_text.py
@@ -1,30 +1,30 @@
 """Разные исходы для rand"""
 
-good_titles = [
+bad_titles = [
     "Вам пизда черти ебаные",
     "Не играть сегодня"
 ]
 
-bad_titles = [
-"Играйте пб умнички ;3"
+good_titles = [
+    "Играйте пб умнички ;3"
 ]
 
-good_description = [
+bad_description = [
     "(Даже не пытайтесь дебики)",
     "(6 сливов подряд)",
 ]
 
-bad_descriptions = [
+good_descriptions = [
     "(Попробуйте, хуже не будет ;3)"
 ]
 
-good_images = [
+bad_images = [
     'https://cdn.discordapp.com/attachments/1101524758824230935/1371488190891950120/'
     'image.png?ex=68235169&is=6821ffe9&hm=0e21de457bf4c13466a6cf4129843ba8023ab2313f'
     'da5fe585e8a178cbca1ac1&'
 ]
 
-bad_images = [
+good_images = [
     'https://cdn.discordapp.com/attachments/1266472308235571261/1309271448094773299/'
     'a922aa26e575735d59359ce3d3ab6cf9.png?ex=6740f98e&is=673fa80e&hm=73d524bae14997a'
     'c28a0f5a2a673e5cce3ad7765a0365743043c20b9446914fc&',
