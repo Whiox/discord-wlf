19807083959300abe1473c7eec870a6478db17a5
Мологин Фёдор <proterrarea@gmail.com>
2025-05-13 12:59:40 +0300

    Add user get methods

--- thunderget.py
+++ thunderget.py
@@ -2,8 +2,8 @@ import requests
 
 
 def squadron_search(name: str, count: int = 1) -> dict:
-    """Поиск полка по названию"""
-    url = f'https://api.thunderinsights.dk/v1/clans/direct/clan/search/?clan={name}&limit=1'
+    """Поиск полка по названию."""
+    url = f'https://api.thunderinsights.dk/v1/clans/direct/clan/search/?clan={name}&limit={count}'
 
     response = requests.get(url)
 
@@ -21,6 +21,49 @@ def squadron_search(name: str, count: int = 1) -> dict:
         'tag': response['clan']['lastPaidTag'],
         'slogan': response['clan']['slogan'],
         'member_count': response['clan']['members_cnt']
+    }
+
+    return context
+
+
+def user_search(username: str, count: int = 2) -> dict:
+    """Поиск пользователя по никнейму."""
+    url = f'https://api.thunderinsights.dk/v1/users/direct/search/?nick={username}&limit={count}'
+
+    response = requests.get(url)
+
+    response = response.json()
+
+    try:
+        response['userid']
+    except TypeError:
+        response = response[0]
+
+    context = {
+        'name': response['nick'],
+        'id': response['userid'],
+        'clan_tag': response['clan_tag'],
+        'clan_name': response['clan_name']
+    }
+
+    return context
+
+
+def get_user(user_id: int) -> dict:
+    """Поиск пользователя по user_id."""
+    url = f'https://api.thunderinsights.dk/v1/users/direct/terse/?userid={user_id}'
+
+    response = requests.get(url)
+
+    response = response.json()
+
+    response = response[str(user_id)]
+
+    context = {
+        'name': response['nick'],
+        'id': str(user_id),
+        'clan_Tag': response['clanTag'],
+        'clan_name': response['clanName']
     }
 
     return context
